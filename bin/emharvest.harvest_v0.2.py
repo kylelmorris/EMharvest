356211fb5f492e8bdfc71b39312c98b72938a19f
amudha <amudha@ebi.ac.uk>
2024-05-16 10:37:49 +0100

    created cif file

--- bin/emharvest.harvest_v0.2.py
+++ bin/emharvest.harvest_v0.2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import os
+import os, re
 import fnmatch
 import math
 import argparse
@@ -9,6 +9,7 @@ import dateutil.parser
 import glob
 from pathlib import Path
 import pandas as pd
+import numpy as np
 
 import json
 from rich.pretty import pprint
@@ -18,6 +19,10 @@ import xmltodict
 import hashlib
 
 import gemmi
+
+from mmcif.api.DataCategory import DataCategory
+from mmcif.api.PdbxContainers import DataContainer
+from mmcif.io.PdbxWriter import PdbxWriter
 
 parser = argparse.ArgumentParser(description="Microscopy Data harvest Script")
 parser.add_argument("-e", "--epu", help="EPU session file: Session.dm")
@@ -730,41 +735,60 @@ def deposition_file(xml):
     model = data["microscopeData"]["instrument"]["InstrumentModel"]
     eV = data["microscopeData"]["gun"]["AccelerationVoltage"]
 
+    microscope_mode = data["microscopeData"]["optics"]["ColumnOperatingTemSubMode"]
+
+    grid_type = df_lookup(main.masterdf, 'gridType')
+    grid_parts = re.findall(r'[A-Z][a-z]*', grid_type)
+
+    # Now, parts will be ['Holey', 'Carbon']
+    grid_toplogy = grid_parts[0]
+    grid_material = grid_parts[1]
+
     # Save doppio deposition csv file
     dictHorizontal1 = {
     'Microscope': model,
     'epuversion': df_lookup(main.masterdf, 'epuVersion'),
-    'date': df_lookup(main.masterdf, 'sessionDate'),
+    'date': df_lookup(main.masterdf, 'sessionDate').strftime("%Y-%m-%d %H:%M:%S"),
     'eV': eV,
     'mag': xmlMag,
     'apix': str(xmlAPix),
     'nominal_defocus_min_microns': df_lookup(main.masterdf, 'defocusMin'),
+    'grid_topology': grid_toplogy,
+    'grid_material': grid_material,
     'nominal_defocus_max_microns': df_lookup(main.masterdf, 'defocusMax'),
     'spot_size': xml_presets.spot,
     'C2_micron': xml_presets.C2,
     'Objective_micron': str(xml_presets_data.objective),
     'Beam_diameter_micron': xml_presets.beamD,
     'collection': df_lookup(main.masterdf, 'afisMode'),
-    'number_of_images': main.mic_count
+    'number_of_images': main.mic_count,
+    'software_name': "EPU",
+    'software_category': "IMAGE ACQUISITION",
+    'microscope_mode': microscope_mode
     }
     df1 = pd.DataFrame([dictHorizontal1])
 
     # Sample data for the second row
     dictHorizontal2 = {
-    'Microscope': '_em_imaging.microscope_model',
-    'epuversion': '_em_software.name',
-    'date': '_em_imaging.date',
-    'eV': '_em_imaging.accelerating_voltage',
-    'mag': '_em_imaging.nominal_magnification',
+    'Microscope': 'em_imaging.microscope_model',
+    'software_name': 'em_software.name',
+    'epuversion': 'em_software.version',
+    'software_category': 'em_software.category',
+    'date': 'em_imaging.date',
+    'eV': 'em_imaging.accelerating_voltage',
+    'mag': 'em_imaging.nominal_magnification',
     'apix': '?',
-    'nominal_defocus_min_microns': '_em_imaging.nominal_defocus_min',
-    'nominal_defocus_max_microns': '_em_imaging.nominal_defocus_max',
+    'nominal_defocus_min_microns': 'em_imaging.nominal_defocus_min',
+    'nominal_defocus_max_microns': 'em_imaging.nominal_defocus_max',
     'spot_size': '?',
-    'C2_micron': '_em_imaging.c2_aperture_diameter',
+    'C2_micron': 'em_imaging.c2_aperture_diameter',
     'Objective_micron': '?',
     'Beam_diameter_micron': '?',
     'collection': '?',
-    'number_of_images': '?'
+    'number_of_images': '?',
+    "microscope_mode": 'em_imaging.mode',
+    "grid_material": 'em_support_film.material',
+    "grid_topology": 'em_support_film.topology'
     }
     df2 = pd.DataFrame([dictHorizontal2])
 
@@ -793,6 +817,16 @@ def deposition_file(xml):
 
     # Deposition Checksum
     checksum(depfilepath, checksumpath)
+
+    # Input data as cif dictionary
+    cif_dict = {}
+
+    for key in dictHorizontal2:
+        cif_dict[dictHorizontal2[key]] = dictHorizontal1[key]
+
+    # transalating and writting to cif file
+    print("CIF_DICTIONARY", cif_dict)
+    translate_xml_to_cif(cif_dict, main.sessionName)
 
 def df_lookup(df, column):
     """
@@ -844,6 +878,106 @@ def perform_minimal_harvest(xml_path, output_dir):
     # Create a deposition file
     deposition_file(xml_path)
 
-    # Create an mmCIF file
+def write_mmcif_file(data_list):
+    """
+    pdbx writer is used to write data stored in self.__dataList
+    :return written: a boolean; True when pdf writer is finished
+    """
+    written = False
+    depfilepath = main.dep_dir + '/' + main.sessionName
+    if depfilepath:
+        mmcif_filename = depfilepath + '_dep.cif'
+        with open(mmcif_filename, "w") as cfile:
+            pdbx_writer = PdbxWriter(cfile)
+            pdbx_writer.write(data_list)
+        written = True
+    return written
+
+def add_container(data_list, container_id):
+    """
+    Adds a container with the specified container_id to the data_list.
+    """
+    container = DataContainer(container_id)
+    data_list.append(container)
+    return container
+
+def add_category(container, category_id, items):
+    """
+    Adds a category with the specified category_id and items to the container.
+    """
+    category = DataCategory(category_id)
+    for item in items:
+        category.appendAttribute(item)
+    container.append(category)
+
+def insert_data(container, category_id, data_list):
+    """
+    Inserts data_list into the specified category_id within the container.
+    """
+    cat_obj = container.getObj(category_id)
+    if cat_obj is None:
+        return
+
+    if all(isinstance(i, list) for i in data_list):
+        list_values = [list(t) for t in zip(*data_list)]
+        cat_obj.extend(list_values)
+    else:
+        cat_obj.append(data_list)
+
+def translate_xml_to_cif(input_data, sessionName):
+    """
+    Translates input XML data into a CIF file.
+    """
+    if not input_data:
+        return False
+
+    input_data_filtered = {key: value for key, value in input_data.items() if key != "?"}
+    cif_data_list = []
+
+    # Create a dictionary to store container_id with corresponding categories and values
+    container_dict = {}
+
+    # Iterate over the input data and accumulate category names and values
+    for key, value in input_data_filtered.items():
+        if isinstance(key, str) and key != "?":
+            container_id, category = key.split(".")
+            if container_id not in container_dict:
+                container_dict[container_id] = {}
+            if category not in container_dict[container_id]:
+                container_dict[container_id][category] = [value]
+            else:
+                # Handle duplicate categories if needed
+                pass
+
+    # Add all accumulated categories and values to the CIF container
+    container = add_container(cif_data_list, sessionName)
+
+    for category_name, categories_values in container_dict.items():
+        category_list = []
+        cif_values_list = []
+        for category, cif_values in categories_values.items():
+            if category == "date":
+                cif_values = [cif_values[0].split(" ")[0]]
+            elif category == "accelerating_voltage":
+                cif_values = [int(float(cif_values[0]) / 1000)]
+            elif category == "nominal_defocus_min":
+                cif_values = [int((cif_values[0]) * -1000)]
+            elif category == "nominal_defocus_max":
+                cif_values = [int((cif_values[0]) * -1000)]
+            elif category == "mode":
+                if cif_values[0] == "BrightField":
+                    cif_values = ["BRIGHT FIELD"]
+            elif category == "topology" or category == "material":
+                cif_values = [cif_values[0].upper()]
+
+
+            category_list.append(category)
+            cif_values_list.append(cif_values)
+        add_category(container, category_name, category_list)
+        insert_data(container, category_name, cif_values_list)
+
+    # Write the modified CIF data to a file
+    return write_mmcif_file(cif_data_list)
+
 
 main()
